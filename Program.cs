38505568b980a77ac16db10d3f8a19a058892d21
Rulbed <pathrulbed@gmail.com>
2023-02-28 23:17:13 +0700

    really really finish!

--- Program.cs
+++ Program.cs
@@ -18,30 +18,24 @@ namespace homework1
             }
             Console.WriteLine("Your Password is " + password);
             Console.WriteLine("Input Your Section ( CIA, FBI, NSA):");
-            string section = Console.ReadLine();
+            string? section = Console.ReadLine();
             
             char n1 = password[0];
-            Console.WriteLine(n1);
             int check1 = n1 - '0';
 
             char n2 = password[1];
-            Console.WriteLine(n2);
             int check2 = n2 - '0';
 
             char n3 = password[2];
-            Console.WriteLine(n3);
             int check3 = n3 - '0';
 
             char n4 = password[3];
-            Console.WriteLine(n4);
             int check4 = n4 - '0';
 
             char n5 = password[4];
-            Console.WriteLine(n5);
             int check5 = n5 - '0';
 
             char n6 = password[5];
-            Console.WriteLine(n6);
             int check6 = n6 - '0';
             
             
